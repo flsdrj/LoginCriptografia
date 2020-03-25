5ac8a24aff90f5e90912b4908c04eb77250be71a
flsdrj <flsd_rj@hotmail.com>
2020-03-24 21:37:00 -0300

    colocando query para banco

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿Create table Perfil(
+IdPerfil	integer			identity(1,1),
+Nome		nvarchar(50)	not null,
+primary key(IdPerfil))
+
+Create table Usuarios(
+IdUsuario	integer			identity(1,1),
+Nome		nvarchar(150)	not null,
+Email		nvarchar(100)	not null unique,
+Senha		nvarchar(50)	not null,
+DataCriacao	datetime		not null,
+[Status]	integer			not null,
+IdPerfil	integer			not null,
+primary key(IdUsuario),
+foreign Key(IdPerfil) references Perfil(IdPerfil))
